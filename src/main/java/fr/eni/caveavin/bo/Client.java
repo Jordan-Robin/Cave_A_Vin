47df0e19be7bd3633d84572578658a6b5e0ba9de
JordanRobin <jordanrobin100@gmail.com>
2025-07-29 21:12:47 +0200

    Création de la classe Client

---
+++ src/main/java/fr/eni/caveavin/bo/Client.java
@@ -0,0 +1,36 @@
+package fr.eni.caveavin.bo;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "CAV_CLIENTS")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @EqualsAndHashCode.Include
+    @Column(name = "LOGIN", nullable = false, unique = true, length = 100)
+    private String pseudo;
+
+    @Column(name = "LAST_NAME", nullable = false, length = 100)
+    private String nom;
+
+    @Column(name = "FIRST_NAME", nullable = false, length = 100)
+    private String prenom;
+
+    @ToString.Exclude
+    @Column(name = "PASSWORD", nullable = false, length = 100)
+    private String password;
+
+}
